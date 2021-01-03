08878d10198268bf0842619c37d0dbbe7818969d
avinashbiradar <avinashbiradar179@gmail.com>
2021-01-03 23:55:56 +0530

    Added:Created a queue

---
+++ src/main/java/MyQueue.java
@@ -0,0 +1,15 @@
+public class MyQueue {
+    private final MyLinkedList myLinkedList;
+    public MyQueue() {
+        this.myLinkedList = new MyLinkedList();
+    }
+    public void queue(INode myNode) {
+        myLinkedList.addElement(myNode);
+    }
+    public void printQueue() {
+        myLinkedList.printMyNodes();
+    }
+    public INode peak() {
+        return myLinkedList.head;
+    }
+}
