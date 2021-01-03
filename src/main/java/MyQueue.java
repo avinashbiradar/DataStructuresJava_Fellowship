787e044d30b37f068d4d539b7f25a96c45e28f61
avinashbiradar <avinashbiradar179@gmail.com>
2021-01-03 23:58:35 +0530

    Added:Dequeing the queue

--- src/main/java/MyQueue.java
+++ src/main/java/MyQueue.java
@@ -12,4 +12,7 @@ public class MyQueue {
     public INode peak() {
         return myLinkedList.head;
     }
+    public INode dequeue() {
+        return myLinkedList.pop();
+    }
 }
--- src/test/java/MyQueueTest.java
+++ src/test/java/MyQueueTest.java
@@ -16,6 +16,19 @@ public class MyQueueTest {
         INode peak=myQueue.peak();
         Assert.assertEquals(peak, myFirstNode);
     }
+    @Test
+    public void given3NumbersWhenDeletePeakValueOfQueueShouldHaveReturnTrue() {
+        KeyNode<Integer> myFirstNode=new KeyNode<>(56);
+        KeyNode<Integer> mySecondNode=new KeyNode<>(30);
+        KeyNode<Integer> myThirdNode=new KeyNode<>(70);
+        myQueue.queue(myFirstNode);
+        myQueue.queue(mySecondNode);
+        myQueue.queue(myThirdNode);
+        INode peak=myQueue.dequeue();
+        myQueue.printQueue();
+        Assert.assertEquals(peak, myFirstNode);
+    }
+
 
 
 }
