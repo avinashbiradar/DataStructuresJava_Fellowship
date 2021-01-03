fdcad86fdb371c59f7f501382cb1c65c26b5d632
avinashbiradar <avinashbiradar179@gmail.com>
2021-01-03 22:31:19 +0530

    Added:Create LinkedList by adding 30 and 56 to 70

---
+++ src/test/java/MyLinkedListTest.java
@@ -0,0 +1,35 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MyLinkedListTest {
+    MyLinkedList mylinkedList=new MyLinkedList();
+    @Test
+    public void given3NumbersShouldPassLinkedListTest() {
+        KeyNode<Integer> myFirstNode=new KeyNode<>(56);
+        KeyNode<Integer> mySecondNode=new KeyNode<>(30);
+        KeyNode<Integer> myThirdNode=new KeyNode<>(70);
+        myFirstNode.setNext(mySecondNode);
+        mySecondNode.setNext(myThirdNode);
+        boolean result = myFirstNode.getNext().equals(mySecondNode) &&
+                mySecondNode.getNext().equals(myThirdNode);
+        Assert.assertTrue(result);
+        System.out.println(myFirstNode.toString());
+    }
+    @Test
+    public void given3NumbersWhenLinkedShouldPasseLinkedListTest() {
+        KeyNode<Integer> myFirstNode=new KeyNode<>(56);
+        KeyNode<Integer> mySecondNode=new KeyNode<>(30);
+        KeyNode<Integer> myThirdNode=new KeyNode<>(70);
+        mylinkedList.add(myThirdNode);
+        mylinkedList.add(mySecondNode);
+        mylinkedList.add(myFirstNode);
+        boolean result=mylinkedList.head.equals(myFirstNode)
+                &&mylinkedList.head.getNext().equals(mySecondNode)
+                &&mylinkedList.tail.equals(myThirdNode);
+        Assert.assertTrue(result);
+        mylinkedList.printMyNodes();
+    }
+
+}
