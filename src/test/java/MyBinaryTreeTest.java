1abe1e252a21931d8ed9ad577e8acc77727538f8
avinashbiradar <avinashbiradar179@gmail.com>
2021-01-04 08:45:10 +0530

    Added:Size method to add more than 3 nodes

--- src/test/java/MyBinaryTreeTest.java
+++ src/test/java/MyBinaryTreeTest.java
@@ -2,6 +2,7 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class MyBinaryTreeTest {
+    MyBinaryTree<Integer> binaryTree = new MyBinaryTree<>();
     @Test
     public void given3NumbersWhenAddedToBinarySearchTreeShouldReturnSizeIsTrue() {
         MyBinaryTree<Integer> binaryTree = new MyBinaryTree<>();
@@ -10,5 +11,23 @@ public class MyBinaryTreeTest {
         binaryTree.add(70);
         int sizeOfBinaryTree = binaryTree.getSize();
         Assert.assertEquals(3, sizeOfBinaryTree);
+    }
+    @Test
+    public void givenManyNumbers_whenAddedToBST_shouldReturnHeightOfTree() {
+        binaryTree.add(56);
+        binaryTree.add(30);
+        binaryTree.add(70);
+        binaryTree.add(22);
+        binaryTree.add(40);
+        binaryTree.add(60);
+        binaryTree.add(95);
+        binaryTree.add(11);
+        binaryTree.add(65);
+        binaryTree.add(3);
+        binaryTree.add(16);
+        binaryTree.add(63);
+        binaryTree.add(67);
+        int sizeOfBST = binaryTree.getSize();
+        Assert.assertEquals(13, sizeOfBST);
     }
 }
