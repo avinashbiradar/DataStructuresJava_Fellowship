9012c3e8cf2f7e52d450ef69a8d04e379fe346c9
avinashbiradar <avinashbiradar179@gmail.com>
2021-01-04 08:06:04 +0530

    Added:Getting frequency of word in paragraph using HashMap with LinkedList

--- src/main/java/MyHashMap.java
+++ src/main/java/MyHashMap.java
@@ -4,10 +4,12 @@ public class MyHashMap<K,V>{
     public MyHashMap() {
         this.myLinkedList=new MyLinkedList<>();
     }
+    //Method for getting value for a particular key
     public V get(K key) {
         MyMapNode<K,V> myMapNode=(MyMapNode<K,V>)this.myLinkedList.search(key);
         return (myMapNode==null)?null:myMapNode.getValue();
     }
+    //Method for add key value pair in HashMap
     public void add(K key,V value) {
         MyMapNode<K,V> myMapNode=(MyMapNode<K,V>)this.myLinkedList.search(key);
 
